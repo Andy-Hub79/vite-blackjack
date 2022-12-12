bea29275163cfea81590ebdba23dfb17b9eb53ab
andy-hub79 <andreszma75@gmail.com>
2022-12-12 01:01:29 -0600

    First commit

---
+++ src/blackjack/usecases/valor-carta.js
@@ -0,0 +1,15 @@
+// pedirCarta();
+
+/**
+ * Obtiene el valor de la carta
+ * @param {String} carta 
+ * @returns {Numer} valor de carta
+ */
+
+export const valorCarta = ( carta ) => {
+ 
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
